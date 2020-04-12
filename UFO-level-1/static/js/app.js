d4cdb9e79a143d54a0401e1e8535eec419cae53c
Shadi <shadi.askarzadeh@gmail.com>
2020-04-11 19:14:56 -0700

    homework upload

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,45 @@
+
+//Pull data from data.js
+const tableData = data;
+
+// use the d3.select to get table references
+const tbody = d3.select("tbody");
+
+function buildTable(data) {
+// First, clear out any existing data
+  tbody.html("");
+
+// Next, loop through each object in the data and append a row and cells for each value in the row and append it to the table body
+  data.forEach((dataRow) => {
+    const row = tbody.append("tr");
+
+// Loop through each field in the dataRow and add each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+        cell.text(val);
+      }
+    );
+  });
+}
+
+function handleClick() {
+
+// Get the datetime value from the filter
+  const date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+
+// Check to see if a date was entered and filter the data using that date.
+// Apply `filter` to the table data to only keep the rows where the `datetime` value matches the filter value
+  if (date) {  
+    filteredData = filteredData.filter(row => row.datetime === date);
+  }
+
+// Rebuild the table using the filtered data
+  buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
